a64f429ca5eabdbd3de03c9a31e8164437ec7ea6
david-m10 <musialik.przemek@gmail.com>
2019-03-30 09:45:21 +0100

    Add express.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const http = require('http');
+const app = require('./backend/app');
+const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.end('First response');
-});
+app.set('port', port);
 
-server.listen(process.env.PORT || 3000);
+const server = http.createServer(app);
+
+server.listen(port);
